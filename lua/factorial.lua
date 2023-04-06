ca28f71ecb45dd19619f32e4b32d954ea4eaa2ae
patrickm663 <rev0cation2025@outlook.com>
2023-04-06 21:26:42 +0200

    Lua: refactor factorial function

--- lua/factorial.lua
+++ lua/factorial.lua
@@ -1,10 +1,11 @@
 local n = 20
 
-function factorial_rec(n)
+function factorial(n)
   if n == 0 then
     return 1
+  else
+    return n * factorial(n-1)
   end
-  return n * factorial_rec(n-1)
 end
 
-print(factorial_rec(n))
+print(factorial(n))
