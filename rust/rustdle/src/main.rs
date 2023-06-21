b4902196470d00f9c6ac380a241e61d9a602a3e5
patrickm663 <rev0cation2025@outlook.com>
2023-06-21 09:16:51 +0200

    Rust: start on Rustdle

--- rust/rustdle/src/main.rs
+++ rust/rustdle/src/main.rs
@@ -1,3 +1,47 @@
+// A simple guessing game
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rustdle");
+
+    // Set up game
+
+    let rand_word_idx = rand::thread_rng()
+        .gen_range(1..=max_num);
+   
+    // Loop until the player wins or quits
+    loop {
+        // Take in user input are parse it as an unsigned 32-bit int
+        println!("Input your guess");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("That's not a number! \n");
+                    continue;
+                }
+            };
+
+        // Debugging
+        println!("You guessed: {guess} \n");
+
+        // Check the guess vs the answer
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("Too low \n"),
+            Ordering::Greater => println!("Too high \n"),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            }
+        }
+    }
 }
+
