9f40b249edd079bee954008d8717b2a5eb2b6a78
patrickm663 <rev0cation2025@outlook.com>
2023-07-02 20:18:33 +0200

    Rust: Some Rustdle updates

---
+++ rust/rustdle/target/debug/deps/rustdle-d504d7b3c1595300.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/sandbox/rust/rustdle/target/debug/deps/rustdle-d504d7b3c1595300: src/main.rs
+
+/data/data/com.termux/files/home/sandbox/rust/rustdle/target/debug/deps/rustdle-d504d7b3c1595300.d: src/main.rs
+
+src/main.rs:
